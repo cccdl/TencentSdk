8e551a0bb1408a8479f771441707eae8ab745955
cccdl <33678639+cccdl@users.noreply.github.com>
2021-02-24 14:40:01 +0800

    修改已有key
    修改key

--- src/examples/comfig.php
+++ src/examples/comfig.php
@@ -1,10 +1,10 @@
 <?php
 
 //IM appid
-$appId = '1400308341';
+$appId = '';
 
 //IM key
-$key = '9f1bdccf37831fa09463e92e93b9944ce92c970c29377182435307407b50bc05';
+$key = '';
 
 // 用户id
 $identifier = 'long';
